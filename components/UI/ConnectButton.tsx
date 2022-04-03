62ef3c280685ddea6bd193b17348c80f674261ac
Tsunfire <underrated@gmail.com>
2022-04-03 04:15:10 -0700

    ⚙️ fix error

--- components/UI/ConnectButton.tsx
+++ components/UI/ConnectButton.tsx
@@ -8,24 +8,30 @@ const ConnectButton = () => {
   useEffect(() => setMounted(true), []);
 
   return (
-    <div className="flex flex-row space-x-4">
-      {connectData.connectors.map((x) => (
-        <button
-          disabled={isMounted ? !x.ready : false}
-          key={x.id}
-          onClick={() => connect(x)}
-          className="border bg-white px-3 py-2 text-black transition duration-300 ease-linear hover:border hover:border-white hover:bg-transparent hover:text-white"
-        >
-          <p>
-            {isMounted ? x.name : x.id === "injected" ? x.id : x.name}
-            {isMounted ? !x.ready && " (unsupported)" : ""}
-          </p>
-        </button>
-      ))}
+    <>
+      <div className="mb-2 flex flex-row space-x-4">
+        {connectData.connectors.map((x) => (
+          <button
+            disabled={isMounted ? !x.ready : false}
+            key={x.id}
+            onClick={() => connect(x)}
+            className="border bg-white px-3 py-2 text-black transition duration-300 ease-linear hover:border hover:border-white hover:bg-transparent hover:text-white"
+          >
+            <p>
+              {isMounted ? x.name : x.id === "injected" ? x.id : x.name}
+              {isMounted ? !x.ready && " (unsupported)" : ""}
+            </p>
+          </button>
+        ))}
+      </div>
       {connectError && (
-        <div>{connectError?.message ?? "Failed to connect"}</div>
+        <div className="w-full">
+          <p className="text-red-600">
+            {connectError?.message ?? "Failed to connect"}
+          </p>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 

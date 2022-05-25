9265f087965cb7e3e3696ad39beb3d81f5647ce6
soju <underrated@gmail.com>
2022-05-24 18:16:55 -0700

    🌈 rainbowkit!!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,66 +1,41 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
-import { Provider, chain, defaultChains } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { WalletLinkConnector } from "wagmi/connectors/walletLink";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiProvider } from "wagmi";
 import { providers } from "ethers";
 import { AnimatePresence } from "framer-motion";
 
-// API key for Ethereum node
-// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
-const infuraId = process.env.INFURA_ID;
-const alchemyId = process.env.ALCHEMY_ID;
-const chainId = process.env.CHAIN_ID;
-const contractAddress = process.env.CONTRACT_ADDRESS;
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+);
 
-// Chains for connectors to support
-// Limit support to Rinkeby
-const chains = chainId
-  ? [defaultChains.find((x) => x.id === parseInt(chainId))]
-  : defaultChains;
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
 
-interface WagmiProps {
-  chainId: number;
-}
-
-// Set up connectors
-const connectors = ({ chainId }: WagmiProps) => {
-  const rpcUrl =
-    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
-    chain.mainnet.rpcUrls[0];
-  return [
-    new InjectedConnector({
-      chains,
-      options: { shimDisconnect: true },
-    }),
-    new WalletConnectConnector({
-      options: {
-        infuraId,
-        qrcode: true,
-      },
-    }),
-    new WalletLinkConnector({
-      options: {
-        appName: "Wagmi App",
-        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
-      },
-    }),
-  ];
-};
-
-// Optional provider, defaults to getDefaultProvider(), which falls back to ether.js shared API keys
-// Reccomended to provide API key to minimize disruptions
-const provider = ({ chainId }: WagmiProps) =>
-  new providers.InfuraProvider(chainId, infuraId);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider autoConnect connectors={connectors}>
-      <AnimatePresence>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </Provider>
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <AnimatePresence>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </RainbowKitProvider>
+    </WagmiProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,30 @@
 import Head from "next/head";
 import Main from "../components/layouts/Main";
-import ConnectButton from "../components/UI/ConnectButton";
 import { FaGithub } from "react-icons/fa";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
     <Main>
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="flex flex-col items-center border border-white/20 bg-black px-4 py-4">
-          <p className="mb-4 w-full text-2xl text-white">Wagmi Boilerplate</p>
-          <div className="mb-16 flex w-full flex-row items-center justify-start space-x-4">
-            <FaGithub className="text-white" />
+      <div className="fixed z-50 flex w-full justify-end px-8 pt-10">
+        <ConnectButton />
+      </div>
+      <div className="relative z-10 flex h-screen w-full items-center justify-center">
+        <div className="flex flex-col items-start px-4 py-4">
+          <p className="text-2xl text-black">welcome to</p>
+          <p className="mb-6 text-6xl font-bold text-black">
+            wagmi boilerplate
+          </p>
+          <div className="mb-16 flex flex-row items-center justify-start space-x-4">
+            <FaGithub className="text-black" />
             <a
               href="https://github.com/zkSoju/wagmi-boiler"
               target="_blank"
-              className="w-full cursor-pointer text-white/50 underline underline-offset-1 transition duration-300 ease-linear hover:text-white"
+              className=" cursor-pointer text-black/50 underline underline-offset-1 transition duration-300 ease-linear hover:text-black"
             >
-              <p>wagmi-boiler</p>
+              <p>wagmi boiler</p>
             </a>
           </div>
-
-          <ConnectButton />
         </div>
       </div>
     </Main>

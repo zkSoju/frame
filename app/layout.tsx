22fc2c3c8825a56ec205f8b868d8b319feb12ea9
Tsunfire <underrated@gmail.com>
2022-12-23 12:42:38 -0800

    bump packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "../styles/tailwind.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 

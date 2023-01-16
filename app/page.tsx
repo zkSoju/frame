fb7e2495c56479b731df647e88036b81a54971a6
Tsunfire <underrated@gmail.com>
2023-01-16 09:36:48 -0800

    use tailwind eslint instead of prettier

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div>
       <div className="relative z-10 flex h-screen w-full items-center justify-center">
-        <div className="flex flex-col items-start px-4 py-4">
+        <div className="flex flex-col items-start p-4">
           <p className="mb-6 text-8xl font-medium text-white">
             wagmi boilerplate
           </p>
           <a
             href="https://github.com/zkSoju/wagmi-boiler"
             target="_blank"
-            className="border-1 cursor-pointer rounded-full border border-white/5 bg-white/10 px-2 py-0.5 text-white backdrop-blur-lg transition duration-300 ease-linear hover:text-white"
+            className="cursor-pointer rounded-full border border-white/5 bg-white/10 p-2 text-white backdrop-blur-lg transition duration-300 ease-linear hover:text-white"
+            rel="noreferrer"
           >
             <div className="flex flex-row items-center space-x-2">
               <FaGithub />

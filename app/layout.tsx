302b5f83fd5321223e6c2b138e38d10014038ca7
soju <underrated@gmail.com>
2023-11-01 12:57:07 -0700

    install pino

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,31 @@
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import { Metadata } from "next";
 import { configureChains, createConfig, mainnet, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import "../styles/globals.css";
 import "../styles/tailwind.css";
 import Navbar from "./Navbar";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(""),
+  title: "",
+  description: "",
+  openGraph: {
+    type: "website",
+    title: "",
+    description: "",
+    images: [
+      {
+        url: "https://res.cloudinary.com/honeyjar/image/upload/v1677023883/THJ_WebBanner.jpg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+};
 
 export default function RootLayout({
   children,
